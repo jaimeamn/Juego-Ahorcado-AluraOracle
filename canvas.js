bd0db48a4eca3a6893c9324203c500f387d82360
jaimeamn1 <jaimeamn1@gmail.com>
2022-12-17 15:47:37 -0500

    funcion dibujar ahorcado, perdiste, ganaste, recargar

--- canvas.js
+++ canvas.js
@@ -7,8 +7,8 @@ function dibujarCanvas(){
 
     tablero.fillRect(0,0,1200,860);
     tablero.beginPath();
-    tablero.moveTo(650, 500);
-    tablero.lineTo(900, 500);
+    tablero.moveTo(300, 500);
+    tablero.lineTo(500, 500);
     tablero.stroke();
     tablero.closePath();
 }
@@ -19,14 +19,108 @@ function dibujarLinea(){
     tablero.lineJoin = "round";
     tablero.fillStyle = "#F3F5F6";
     tablero.strokeStyle = "#0A3871";
+    tablero.beginPath()
 
     let anchura = 600/palabraSecreta.length;
     for (let i = 0; i < palabraSecreta.length; i++){
         
-        tablero.moveTo(500 + (anchura*i), 640)
-        tablero.lineTo(550 + (anchura*i), 640)
+        tablero.moveTo(300 + (anchura*i), 640)
+        tablero.lineTo(350 + (anchura*i), 640)
     }
 
     tablero.stroke();
     tablero.closePath();
 }
+
+
+function escribirLetraCorrecta(index){
+    tablero.font = 'bold 52px Inter';
+    tablero.lineWidth = 6;
+    tablero.lineCap = "round";
+    tablero.lineJoin = "round";
+    tablero.fillStyle = "#0A3871";
+
+    let anchura = 600/palabraSecreta.length
+    tablero.fillText(palabraSecreta[index],305+(anchura*index),620)
+    tablero.stroke()
+
+}
+
+function escribirLetraIncorrecta(letra, errorsLeft){
+    tablero.font = 'bold 40px Inter';
+    tablero.lineWidth = 6;
+    tablero.lineCap = "round";
+    tablero.lineJoin = "round";
+    tablero.fillStyle = "#0A3871";
+    tablero.fillText(letra, 200+(40*(10-errorsLeft)), 710,40)
+}
+
+function dibujarAhorcado(puntaje) {
+    tablero.lineWidth=8
+    tablero.lineCap="round"
+    tablero.lineJoin="round"
+    tablero.strokeStyle = "#0A3871"
+    if(puntaje===8){
+    //poste lateral
+    tablero.moveTo(500,500)
+    tablero.lineTo(500,100)
+    }
+    if(puntaje===7){//teto 
+    tablero.moveTo(500,100)
+    tablero.lineTo(700,100)
+    }
+    if(puntaje===6){//corda
+    tablero.moveTo(700,100)
+    tablero.lineTo(700,171)
+    }
+    if(puntaje===5){//para cara
+    tablero.moveTo(750,230)
+    tablero.arc(700,230,50,0,Math.PI*2)
+    }
+    if(puntaje===4){//para corpo
+    tablero.moveTo(700,389)
+    tablero.lineTo(700,289)
+    }
+    if(puntaje===3){//para perna izquerda
+    tablero.moveTo(700,389)
+    tablero.lineTo(650,450)
+    }
+    if(puntaje===2){//para perna direita
+    tablero.moveTo(700,389)
+    tablero.lineTo(740,450)
+    }
+    if(puntaje===1){//para mão izquerda
+    tablero.moveTo(700,330)
+    tablero.lineTo(650,389)
+    }
+    if(puntaje===0){//para mão direita
+    tablero.moveTo(700,330)
+    tablero.lineTo(740,389)
+    }
+    tablero.stroke()
+    tablero.closePath()
+  }
+
+  function perdiste() {
+    tablero.font = ' bold 42px Inter';
+    tablero.lineWidth=6
+    tablero.lineCap="round"
+    tablero.lineJoin="round"
+    tablero.fillStyle="red"
+    tablero.fillText("Fin del juego!",930,320)
+  }
+
+  function ganaste() {
+    tablero.font = 'bold 42px Inter';
+    tablero.lineWidth=6
+    tablero.lineCap="round"
+    tablero.lineJoin="round"
+    tablero.fillStyle="green"
+    tablero.fillText("Ganaste,",950,320)
+    tablero.fillText("Felicidades!",930,360)
+    setTimeout( recargar , 1000)
+  }   
+
+  function recargar(){
+    location.reload(); 
+  }
--- horca.js
+++ horca.js
@@ -1,7 +1,24 @@
 //Selectores
+//Seletores
+let pantalla = document.querySelector("canvas");
+let botonNuevoJuego = document.getElementById("btn-nuevo-juego").style.display = "none"
+let btnSalirDesaparecer = document.getElementById("btn-salir").style.display = "none"
+let divAgregarPalabra = document.getElementById("agregar-palabra").style.display = 'none';
+let btnNuevoJuego = document.getElementById("btn-nuevo-juego");
+let btnSalir = document.getElementById("btn-salir");
+let btnCancelar = document.getElementById("btn-cancelar");
+
+
+
 let palabras = ["ALURA", "ORACLE", "ONE", "JAVASCRIPT","HTML", "CSS"];
 let tablero = document.getElementById("forca").getContext("2d");
 let palabraSecreta = "";
+var palabraCorrecta = "";
+let letras = [];
+let errores = 8;
+let letrasIncorrectas = [];
+let numeroDeErrores = 8
+let letraElegida = [];
 
 //PalabraSecreta
 
@@ -9,9 +26,43 @@ function escojerPalabraSecreta(){
     let palabra = palabras[Math.floor(Math.random() * palabras.length)]
     palabraSecreta = palabra
     console.log(palabraSecreta)
+    return palabra
+    
 }
 
+// verifica cual es la letra en que el usuario hizo clic
+function verificarLetraClicada(key) {
+    if (letras.length < 1 || letras.indexOf(key) < 0) {
+      letras.push(key)
+      return false
+      
+    }
+    else {
+      letras.push(key)
+      return true
+    }
+  }
 
+  //impide que teclas como shift y otras, sean consideradas errores y sean escritas
+function verificarLetra(keyCode) {
+    if (typeof keyCode === "number" && keyCode >= 65 && keyCode <= 90) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+
+function añadirLetraCorrecta(i) {
+    palabraCorrecta += palabraSecreta[i].toUpperCase()
+  }
+
+function anadirLetraIncorrecta(letra){
+    if (palabraSecreta.indexOf(letra) <= 0) {
+        errores -= 1
+      }
+    console.log(errores)
+}
 
 //Iniciar juego
 function iniciarJuego(){
@@ -20,4 +71,97 @@ function iniciarJuego(){
     escojerPalabraSecreta()
     dibujarCanvas()
     dibujarLinea()
+
+     // hace con que los botones de nuevo juego e salir aparezcan
+    document.getElementById("btn-nuevo-juego").style.display = "block"
+    document.getElementById("btn-salir").style.display = "block"
+
+    // captura la letra que el usuario escribió
+    document.onkeydown = (e) => {
+        // pone la letra en letra mayuscula
+        let letra = e.key.toUpperCase()
+        //verifica si el usuario todavia no ha perdido
+        if (letrasIncorrectas.length <= numeroDeErrores) {
+        if (!verificarLetraClicada(e.key) && verificarLetra(e.keyCode)) {
+            if (palabraSecreta.includes(letra)) {
+            añadirLetraCorrecta(palabraSecreta.indexOf(letra))
+            for (let i = 0; i < palabraSecreta.length; i++) {
+                if (palabraSecreta[i] === letra) {
+                escribirLetraCorrecta(i)
+                verificarVencedor(letra)
+
+                }
+            }
+
+            }
+            // si el usuario cometió más errores de los que son permitidos, 
+            //llama las funciones que dibujan el ahorcado y exibe el mensaje de fin de juego
+            else {
+            if (!verificarLetraClicada(e.key) && !verificarVencedor(letra)) return
+            dibujarAhorcado(errores)
+            verificarFinJuego(letra)
+            }
+        }
+        }
+        else {
+        alert('has atingido el límite de letras incorrectas')
+        }
+
+    };
 }
+function verificarFinJuego(letra) {
+    //checa si la letra ha sido incluída en el array de  las letras correctas o incorrectas
+   if(letraElegida.length < palabraSecreta.length) { 
+      //incluye las letras ya digitadas en el arrau
+      letrasIncorrectas.push(letra);
+      
+  
+      //valida se el usuário cometió el numero maximo de errores
+      if (letrasIncorrectas.length > numeroDeErrores) {
+        perdiste()
+      }
+      else if(letraElegida.length < palabraSecreta.length) {
+        anadirLetraIncorrecta(letra)
+        escribirLetraIncorrecta(letra, errores)
+      }
+    }
+   } 
+  
+  //Verifica si el usuario ha ganado
+  function verificarVencedor(letra) {
+    letraElegida.push(letra.toUpperCase());
+    if (letraElegida.length == palabraSecreta.length) {
+  
+      ganaste()
+      
+    }
+  
+  }
+  //haz con que los botones de la pantalla de home desaparezcan y los de la de agregar palabra aparezcan
+function ensenarPantallaDeAgregarPalabra() {
+    document.getElementById("div-desaparece").style.display = 'none';
+    document.getElementById("agregar-palabra").style.display = "block";
+  
+  }
+  
+  // guarda la palabra que el usuario quiere agregar
+  function guardarPalabra() {
+    
+    //captura lo que el usuario ha digitado
+    let nuevaPalabra = document.getElementById('input-nueva-palavra').value;
+  
+    // incluye la palabra que el usuario digitó en el array de las palabras a seren sorteadas
+    if(nuevaPalabra !== ""){
+      palabras.push(nuevaPalabra.toUpperCase());
+      alert('La palabra fue guardada')
+      
+    
+      // haz con que los componentes de la pantalla de agregar palabra desaparezcan
+      document.getElementById("agregar-palabra").style.display = "none";
+      iniciarJuego();
+    }
+    else{
+      alert("Ninguna palabra ha sido digitada")
+    }
+  
+  }
